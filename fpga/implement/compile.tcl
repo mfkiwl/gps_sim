7051f494ecbd04195ced6c770578613720346e5d
hdlguy <hdlguy@hotmail.com>
2019-06-19 11:56:59 -0400

    fixed compile.tcl for microZed

--- fpga/implement/compile.tcl
+++ fpga/implement/compile.tcl
@@ -26,12 +26,8 @@ report_timing_summary -file ./results/post_route_timing_summary.rpt
 report_utilization    -file ./results/post_route_utilization.rpt
 
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [get_designs impl_1]
-#write_bitstream      -force ./results/top.bit
 write_bitstream -bin_file -force ./results/top.bit
 
 close_project
 
-exec bootgen -image bitstream.bif -arch zynqmp -o ./results/top.bit.bin -w
-
-#write_cfgmem -disablebitswap -force -format BIN -size 256 -interface SMAPx32 -loadbit "up 0x0 ./results/top.bit" -verbose ./results/top.bit.bin
 
