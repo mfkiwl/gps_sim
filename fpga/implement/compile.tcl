67221d745b0b49e37b2a39ef50272b8328d79d8a
hdlguy <hdlguy@hotmail.com>
2019-06-19 12:43:13 -0400

    making files for fpga configuration under linux

--- fpga/implement/compile.tcl
+++ fpga/implement/compile.tcl
@@ -26,8 +26,10 @@ report_timing_summary -file ./results/post_route_timing_summary.rpt
 report_utilization    -file ./results/post_route_utilization.rpt
 
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [get_designs impl_1]
-write_bitstream -bin_file -force ./results/top.bit
+#write_bitstream -bin_file -force ./results/top.bit
+write_bitstream -force ./results/top.bit
 
 close_project
 
+exec bootgen -image bitstream.bif -arch zynq -o ./results/top.bit.bin -w
 
