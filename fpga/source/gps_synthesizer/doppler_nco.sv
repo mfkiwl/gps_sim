80184baa206471df7fdc60a7b4f86f1c75801daf
hdlguy <hdlguy@hotmail.com>
2019-06-20 17:26:13 -0400

    starting nco testbench

--- fpga/source/gps_synthesizer/doppler_nco.sv
+++ fpga/source/gps_synthesizer/doppler_nco.sv
@@ -1,16 +1,29 @@
 module doppler_nco (
     input  logic        clk,
     input  logic[31:0]  freq,
-    output logic[15:0]  real_out,
-    output logic[15:0]  imag_out
+    output logic[9:0]   real_out,
+    output logic[9:0]   imag_out
 );
 
+    // Let's use 32 bit accummulators for the DDS.
+    logic [31:0] phase;
+    always_ff @(posedge clk) begin
+        if (enable = 1) then
+            phase <= phase + freq;
+        else
+            phase <= 0;
+        end if
+    end
+
+    logic [31:0] m_axis_data_tdata;
     doppler_rom doppler_rom_inst (
-        .aclk(aclk),                                // input wire aclk
-        .s_axis_phase_tvalid(s_axis_phase_tvalid),  // input wire s_axis_phase_tvalid
-        .s_axis_phase_tdata(s_axis_phase_tdata),    // input wire [15 : 0] s_axis_phase_tdata
-        .m_axis_data_tvalid(m_axis_data_tvalid),    // output wire m_axis_data_tvalid
+        .aclk(clk),                                // input wire aclk
+        .s_axis_phase_tvalid('1'),  // input wire s_axis_phase_tvalid
+        .s_axis_phase_tdata(phase),    // input wire [15 : 0] s_axis_phase_tdata
+        .m_axis_data_tvalid(),    // output wire m_axis_data_tvalid
         .m_axis_data_tdata(m_axis_data_tdata)       // output wire [31 : 0] m_axis_data_tdata
     );
+    assign imag_out = m_axis_data_tdata[16 +: 9];
+    assign real_out = m_axis_data_tdata[ 0 +: 9];
 
 endmodule
